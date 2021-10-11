23f457b9a3ff9e818d32b84d32cd5d14a68ac11b
Dybol <mdyblik01@gmail.com>
2021-10-11 10:26:30 +0200

    SimpleMessageProducer produces today's date

--- Lista 1/src/main/java/me/mikolaj/beans/SimpleMessageProducer.java
+++ Lista 1/src/main/java/me/mikolaj/beans/SimpleMessageProducer.java
@@ -1,11 +1,13 @@
 package me.mikolaj.beans; 
-import org.springframework.stereotype.Component; 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component 
 public class SimpleMessageProducer implements MessageProducer {
 
 	@Override 
 	public String getMessage() {
-		return "Hello World! " + System.currentTimeMillis();
+		return "Hello World! Today is: " + LocalDateTime.now();
 	} 
 }
