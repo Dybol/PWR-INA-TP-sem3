c23bf9d39bf030de42b3788a26c97b88353189fb
Dybol <mdyblik01@gmail.com>
2022-01-03 16:51:45 +0100

    Add unit tests for connection

--- Lista4/src/main/java/me/mikolaj/server/ChineseCheckersServer.java
+++ Lista4/src/main/java/me/mikolaj/server/ChineseCheckersServer.java
@@ -16,8 +16,20 @@ public class ChineseCheckersServer {
 	 *
 	 * @param args - program arguments
 	 */
-	public static void main(final String[] args) throws Exception {
-		try (final ServerSocket listener = new ServerSocket(58901)) {
+	public static void main(final String[] args) {
+		final ChineseCheckersServer server = new ChineseCheckersServer();
+		server.createServer(58901);
+	}
+
+	/**
+	 * Creates a server
+	 *
+	 * @param port - port for the server
+	 */
+	public void createServer(final int port) {
+
+		try {
+			final ServerSocket listener = new ServerSocket(port);
 			System.out.println("Chinese Checkers server is running...");
 			final ExecutorService pool = Executors.newFixedThreadPool(6);
 
@@ -27,6 +39,9 @@ public class ChineseCheckersServer {
 					throw new IllegalStateException("There are too many players!");
 				pool.execute(new Player(listener.accept(), Player.getPlayers().size() + 1));
 			}
+		} catch (final Exception ex) {
+			System.out.println(ex);
+			System.out.println("There were problem with the server!");
 		}
 	}
 }
---
+++ Lista4/src/test/java/ConnectionTest.java
@@ -0,0 +1,75 @@
+import me.mikolaj.client.ChineseCheckersClient;
+import me.mikolaj.client.Player;
+import me.mikolaj.server.ChineseCheckersServer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.Socket;
+
+/**
+ * Test class for connections
+ */
+public class ConnectionTest {
+
+	/**
+	 * Checks if connecting with server was successful
+	 *
+	 * @throws Exception - if anything went wrong
+	 */
+	@Test
+	public synchronized void testConnection() throws Exception {
+		final ChineseCheckersServer server = new ChineseCheckersServer();
+		final Thread t = new Thread(() -> {
+			try {
+				server.createServer(58902);
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		});
+		t.start();
+		new Socket("localhost", 58902);
+	}
+
+	/**
+	 * Checks if players can join successfully
+	 *
+	 * @throws Exception - if anything went wrong
+	 */
+	@Test
+	public synchronized void testPlayerJoin() throws Exception {
+		Thread.sleep(1000);
+		final ChineseCheckersServer server = new ChineseCheckersServer();
+		final Thread t = new Thread(() -> {
+			try {
+				server.createServer(58901);
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		});
+		t.start();
+		final Thread t2 = new Thread(() -> {
+			try {
+				final ChineseCheckersClient client = new ChineseCheckersClient("localhost");
+				client.play();
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		});
+		t2.start();
+		final Thread t3 = new Thread(() -> {
+			try {
+				final ChineseCheckersClient client2 = new ChineseCheckersClient("localhost");
+				client2.play();
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		});
+		t3.start();
+
+		//we add delay so that everything got synchronized
+		Thread.sleep(1000);
+
+		//as we have already one player in because of the previous method
+		Assert.assertEquals(3, Player.getPlayers().size());
+	}
+}
