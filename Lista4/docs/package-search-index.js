fe76e7cee56379bf507dc09b0d274c9705e50ce0
Dybol <mdyblik01@gmail.com>
2022-01-09 16:55:25 +0100

    Generate javadocs

---
+++ Lista4/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.mikolaj.client"},{"l":"me.mikolaj.logic"},{"l":"me.mikolaj.server"},{"l":"me.mikolaj.utils"}];updateSearchResults();
